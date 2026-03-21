6df506e778c2c87f7dc88e22ee6e8eb95b78b879
Shreyas <shreyasjoshi2511@gmail.com>
2026-03-21 12:18:27 +0530

    Solved 09 using Number methods based checking and then computed the integer sum till that point and then subtracted both sums

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if( !Number.isInteger(num1) || !Number.isInteger(num2) || num1 < 0 || num2 < 0 ){
+        return 'ERROR';
+    }  
 
+    if(num1 > num2) return sumAll(num2, num1);
+
+    const sum2 = (num2 * (num2+1) / 2);
+    const sum1 = ((num1-1) * (num1) / 2);
+
+    return sum2-sum1;
 };
 
 // Do not edit below this line
