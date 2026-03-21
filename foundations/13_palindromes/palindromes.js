e09399b81e5e66c6cb853c9b2209d3cf15d9595f
Shreyas Joshi <shreyasjoshi2511@gmail.com>
2026-03-21 23:27:39 +0530

    Solved 13 using two pointer approach and creating custom functions for isAlnum test

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    let left = 0;
+    let right = text.length-1;
 
+    const isAlnum = char => /^[a-z0-9]$/i.test(char);
+
+    while(left < right){
+        while(left < right && !isAlnum(text[left])) left++;
+
+        while(left < right && !isAlnum(text[right])) right--;
+
+        if(text[left].toLowerCase() !== text[right].toLowerCase()) return false;
+
+        left++;
+        right--;
+    }
+
+    return true;
 };
 
 // Do not edit below this line
