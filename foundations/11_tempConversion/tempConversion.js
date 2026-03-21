469a7daac7189775a8480037bd6ba9572bf39546
Shreyas Joshi <shreyasjoshi2511@gmail.com>
2026-03-21 22:58:49 +0530

    Solved 11 using the formula and type casting using + operator and used toFixed to round to 1 decimal place

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,8 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreeF) {
+
+  return +((degreeF - 32) / 1.8).toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreeC) {
+
+  return +((degreeC * 1.8) + 32).toFixed(1);
+
 };
+
 
 // Do not edit below this line
 module.exports = {
