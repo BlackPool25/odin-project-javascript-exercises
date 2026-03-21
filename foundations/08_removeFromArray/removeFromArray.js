868b82dfdde7fd0842625fb1f1071021601ecacf
Shreyas <shreyasjoshi2511@gmail.com>
2026-03-21 12:03:22 +0530

    Solved 08 using filtering and includes method for strict matching

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(sourceArray, ...elementsToRemove) {
+    if(arguments.length == 0) return 'ERROR: There needs to be at least one argument';
+
+    return sourceArray.filter(item => !elementsToRemove.includes(item));
 };
 
 // Do not edit below this line
