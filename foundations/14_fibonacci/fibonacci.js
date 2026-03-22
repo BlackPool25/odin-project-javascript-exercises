eccbd5af16de155bc56b2c858f2bf407f88b1b9a
Shreyas Joshi <shreyasjoshi2511@gmail.com>
2026-03-22 10:03:59 +0530

    Solved foundations 14 using recusion and memorisation to save time and increase efficiency

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num, memo = new Map()) {
+    if(!Number.isInteger(num) || num < 0) return "OOPS";
 
+    if(memo.has(num)){
+        console.log(`Saved time on ${num}`);
+        return memo.get(num);
+    }
+
+    if(num <= 1) return num;
+    
+    memo.set(num, fibonacci(num-1, memo) + fibonacci(num-2, memo));
+
+    return memo.get(num);
 };
+
+console.log(fibonacci(55));
 
 // Do not edit below this line
 module.exports = fibonacci;
