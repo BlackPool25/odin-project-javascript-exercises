451f41fb44ec898023bfdd927123678c384e0f40
Shreyas Joshi <shreyasjoshi2511@gmail.com>
2026-03-22 21:43:42 +0530

    Solved 15 and 16 in foundations using arrow functions and using map inbuilt function to construct the array of titles

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,27 @@
-const findTheOldest = function() {
+const { getVersion } = require("jest");
 
+const findTheOldest = function(arrObj) {
+    if(arrObj.length === 0) return undefined;
+
+    let oldest = [arrObj[0], getAge(arrObj[0])];
+
+    for(let i=1; i<arrObj.length; i++){
+
+        let curAge = getAge(arrObj[i]);
+
+        if(curAge > oldest[1]){
+            oldest[0] = arrObj[i];
+            oldest[1] =  curAge;
+        }
+    }
+
+    return oldest[0];
 };
+
+const getAge = (personObj) => {
+    return (Object.hasOwn(personObj, 'yearOfDeath') ? personObj.yearOfDeath : new Date().getFullYear()) - personObj.yearOfBirth;
+}
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
